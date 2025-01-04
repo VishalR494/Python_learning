f385d407ed38b5106e3b3215400d6b8c0c7c2260
VishalR494 <vishalracheti494@gmail.com>
2025-01-04 18:57:08 +0530

    Committed

---
+++ Hello.py
@@ -0,0 +1,7 @@
+a = 3+2  #Variables
+com = 5+6j
+print("Hello world", a)
+print(type(com))  #Complex
+
+print(com.real)  #Output 5.0
+print(com.imag)  #Output 6.0
---
+++ Print.py
@@ -0,0 +1,9 @@
+
+name= input("Please enter the name:  ")
+a= 5.5
+print ("Your name is", name)   
+#Type casting
+print(type (int(a)))  #a is float but by type casting we converted it into the int
+print(type (bool(a)))
+
+#Note : String cannot be converted to the integer
